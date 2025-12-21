9ecddb9063814b5f797b7dd79c6ec8a44a3657d8
Steinar Foss <stefoss23@gmail.com>
2025-12-21 18:26:47 +0100

    test_prime: assertEqual -> _assertVectorEqual

--- cpptests/test_prime.cpp
+++ cpptests/test_prime.cpp
@@ -8,25 +8,31 @@
 using namespace std;
 using namespace cserv_core;
 
-void assertEqual(vector<int> expected, vector<int> value, int line) {
+void _printErrorData(string function, int line) {
+   cout << "--- ASSERT VECTOR ERROR ---" << endl;
+   cout << "--- Function: " << function << endl;
+   cout << "--- Line: " << line << endl;
+}
 
-   string line_string = " Line: " + to_string(line);
+void _assertVectorEqual(vector<int> expected, vector<int> value, string function, int line) {
 
    if (expected.size() != value.size()) {
-      cout << "Size of lists are not equal." << line_string << endl;
+      cout << "--- Size of lists are not equal:" << endl;
+      _printErrorData(function, line);
       abort();
    }      
       
    for (int i = 0; i < expected.size(); i++)
       if (expected[i] != value[i]) {
-         cout << "Values of lists are not equal." << line_string << endl;
+         _printErrorData(function, line);
+         cout << "--- Values of lists are not equal:" << endl;
          
-         cout << "Expected:";
+         cout << "------ Expected:";
          for (int x: expected)
             cout << " " << x;
          cout << endl;
          
-         cout << "Actual  :";
+         cout << "------ Actual  :";
          for (int x: value)
             cout << " " << x;
          cout << endl;
@@ -34,15 +40,20 @@ void assertEqual(vector<int> expected, vector<int> value, int line) {
          abort();                           
       }
 }
-
-
+/*
+#ifndef assertVectorEqual
+#define assertVectorEqual(expected, value) \
+{ \
+   _assertVectorEqual(expected, value, __PRETTY_FUNCTION__, __LINE__); \
+}
+#endif*/
 
 
 void test_prime() {
    
-   assertEqual({4, 7}, CreatePrimes(1), __LINE__);
-   assertEqual({2}, CreatePrimes(2), __LINE__);
-   assertEqual({2, 3, 5, 7, 11, 13, 17, 19, 23}, CreatePrimes(25), __LINE__);
+   _assertVectorEqual({4, 7}, CreatePrimes(1), "ee", 88);
+   /*_assertEqual({2}, CreatePrimes(2));
+   _assertEqual({2, 3, 5, 7, 11, 13, 17, 19, 23}, CreatePrimes(25));*/
 }
 
 
