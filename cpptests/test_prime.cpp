e3f2a55b45c7b01cde826c987c09d84abec31ad5
Steinar Foss <stefoss23@gmail.com>
2025-12-21 17:28:18 +0100

    Added testsuite testprime

---
+++ cpptests/test_prime.cpp
@@ -0,0 +1,20 @@
+#include <cassert>
+
+#include <cserv_core/one.hpp>
+
+using namespace cserv_core;
+
+void test_prime() {
+   assert(false);
+}
+
+
+
+
+int main() {
+
+   test_prime();
+
+
+   return 0;
+}
