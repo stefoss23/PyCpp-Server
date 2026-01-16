7cebe71ce337ddc4c23473f550c3ba2cb94b9e1b
Steinar Foss <stefoss23@gmail.com>
2026-01-16 17:10:32 +0100

    prime.cpp: added functional code for getting primes

--- src/prime.cpp
+++ src/prime.cpp
@@ -1,3 +1,6 @@
+#include <cmath>
+#include <vector>
+
 #include <cserv_core/prime.hpp>
 
 using namespace std;
@@ -5,7 +8,34 @@ using namespace std;
 namespace cserv_core {
    
    vector<int> CreatePrimes(int max) {
-      return {4, 8};
+   
+      vector<int> list;
+   
+      int limit = sqrt(max);
+   
+      for (int i = 2; i <= max; i++) {
+   
+         bool is_prime = true;
+   
+         for (int j: list) {
+      
+            if (j > limit)
+               break;
+            
+            if (i % j == 0) {
+               is_prime = false;
+               break;
+            }
+         }
+      
+      if (is_prime)
+         list.push_back(i);
+      }    
+
+      return list;      
+   
+   
+      //return {4, 8};
    }
    
 }
