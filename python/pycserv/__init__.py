06a3ce80e0c6de9ce57f34e1454e7f6a18ddc1b5
Steinar Foss <stefoss23@gmail.com>
2026-02-05 01:58:03 +0100

    cpp lib made available in server

--- python/pycserv/__init__.py
+++ python/pycserv/__init__.py
@@ -1,11 +1,14 @@
+from .cserv_python import *
+
 def func():
    return "pycserv function called"
    
 def get_primes(num):
 
-   lst = []
-   
-   for n in range(num):
-      lst.append(n)
+   #lst = []
+   #
+   #for n in range(num):
+   #   lst.append(n)
 
-   return lst 
+   #return lst '
+   return primes(num)
