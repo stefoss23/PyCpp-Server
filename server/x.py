3e27bfc439a5dbc149b9f8879b6552d83f3aede9
Steinar Foss <stefoss23@gmail.com>
2026-02-08 16:31:10 +0100

    x.py: rough example of server test

--- server/x.py
+++ server/x.py
@@ -1,10 +1,10 @@
 import requests
 import multiprocessing
 import subprocess
-
+import time
 
 def serv():
-   subprocess.run(['timeout', '6s', 'python3', 'manage.py', 'runserver'])
+   subprocess.run(['timeout', '20s', 'python3', 'manage.py', 'runserver'])
    print("--- Server finished")
    
    
@@ -12,7 +12,15 @@ p1 = multiprocessing.Process(target=serv)
 
 p1.start()
 
+time.sleep(5)
+
 print("--- Server has started")
+
+url = 'http://127.0.0.1:8000/primes/?num=20'
+
+response = requests.get(url)
+
+print(response.json())
 
 p1.join()
 
