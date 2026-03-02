d8b4f13b1cbec620517c7a54f85cbc4839f3e598
Steinar Foss <stefoss23@gmail.com>
2026-03-02 11:57:44 +0100

    created python function segfault

--- python/cxx/cserv_pybind.cpp
+++ python/cxx/cserv_pybind.cpp
@@ -3,11 +3,13 @@
 #include <pybind11/stl.h>
 
 #include <cserv_core/prime.hpp>
+#include <cserv_core/errors.hpp>
 
 using namespace cserv_core;
 
 PYBIND11_MODULE(cserv_python, m) {
 
   m.def("primes", &CreatePrimes);
-
+  m.def("segfault", &segfault);
+  
 }
